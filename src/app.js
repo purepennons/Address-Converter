bbe8bb18bce5085ff4f2830cb99f57895029a339
purepennons <y80811@gmail.com>
2015-01-17 17:32:48 +0800

    let program accept a param.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,52 @@
-function() {
+(function() {
   "use strict"
 
-  
+  var addToLat = require('./js/addToLat')
 
-}();
+  ;
+
+  var argvs = process.argv;
+
+  if(argvs.length > 2) {
+
+    // accept params
+    var param1 = argvs[2]
+    ;
+
+    switch (param1) {
+
+      case '-i':
+
+        break;
+
+      default:
+        addToLat(param1, function(err, location) {
+
+          if(err) {
+            console.log(err);
+          } else {
+            console.log(param1 + ':', location);
+          }
+
+        });
+
+    }
+
+  } else {
+
+    console.log('Params are not match');
+
+    process.exit(1);
+
+  }
+
+
+
+  //
+  // addToLat('No.108, Sec. 5, Anhe Rd., Annan Dist., Tainan City 709, Taiwan', function(err, location) {
+  //
+  //   console.log(location);
+  //
+  // });
+
+})();
--- src/js/addToLat.js
+++ src/js/addToLat.js
@@ -1,4 +1,5 @@
 module.exports = function(address, cb) {
+  
   "use strict"
 
   var querystring = require('querystring')
